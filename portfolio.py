be3344809ea9de72c8981d396a47dfac71348429
zion379 <zspynet22@icloud.com>
2023-02-28 11:40:22 -0800

    first commit

---
+++ portfolio.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    intro_header_text = "Hello and welcome to my electrical and software engineering portfolio! My name is Zion,"
+    intro_paragraph_text = "and I am a self-taught programmer and electrical engineer with a passion for teaching. Over the years, I have developed expertise in programming languages, game development, and electronics. As a K12 instructor, I have had the opportunity to teach various subjects, including circuit building and micro-controller programming, on behalf of a technical college focused on STEM higher education. In this portfolio, you will find a collection of my projects, ranging from simple circuits to complex software programs, that showcase my technical skills and creativity. I hope that this portfolio serves as an inspiration for young students and aspiring engineers, and that it highlights the exciting possibilities of pursuing a career in electrical and software engineering."
+    return render_template('index.html', intro_header=intro_header_text, intro_paragraph=intro_paragraph_text)
+
+@app.route('/skills')
+def skills():
+    my_skills = ['Python', 'Flask', 'HTML', 'CSS', 'JavaScript', 'CAD', 'Embedded Software', 'Embedded Hardware', 'C Programming', 'Electronics', 'Game Development', 'App Develoment']
+    return render_template('skills.html', skills=my_skills)
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+#testing
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s!' % name 
+
+app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run()
